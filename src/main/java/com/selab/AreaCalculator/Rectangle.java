c51c82e1fa09652bbe81fa30da239192971f0ba9
Kiarash Golzadeh <kiarash.gl@gmail.com>
2021-03-17 01:51:18 +0330

    TDD Round 1: Refactor Rectangle class code

--- src/main/java/com/selab/AreaCalculator/Rectangle.java
+++ src/main/java/com/selab/AreaCalculator/Rectangle.java
@@ -1,9 +1,13 @@
 package com.selab.AreaCalculator;
 
 public class Rectangle {
+	private final int width;
+	private final int height;
+
 	public Rectangle(int width, int height) throws IllegalArgumentException {
 		if (width <= 0 || height <= 0)
 			throw new IllegalArgumentException("Width or height should be positive.");
+
 		this.width = width;
 		this.height = height;
 	}
@@ -12,7 +16,4 @@ public class Rectangle {
 	{
 		return width * height;
 	}
-
-	int width;
-	int height;
 }
