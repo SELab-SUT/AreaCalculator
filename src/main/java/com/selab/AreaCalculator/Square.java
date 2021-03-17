97c4400c5fc3e11eeed2af8ee672e300b7b7b0e2
Kiarash Golzadeh <kiarash.gl@gmail.com>
2021-03-17 22:43:17 +0330

    TDD Round 3: Refactor Square class

--- src/main/java/com/selab/AreaCalculator/Square.java
+++ src/main/java/com/selab/AreaCalculator/Square.java
@@ -4,9 +4,7 @@ public class Square implements AreaComputable {
 	private int side;
 
 	public Square(int side) throws IllegalArgumentException {
-		if (side <= 0)
-			throw new IllegalArgumentException("Side should be positive.");
-		this.side = side;
+		setSide(side);
 	}
 
 	public int getSide() {
