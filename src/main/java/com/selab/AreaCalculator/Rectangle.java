eeb73f9d87e9b3de68dd8b8f02bbc88a0a76342e
Alireza Mousavi <moosavi78@gmail.com>
2021-03-17 04:03:25 +0330

    TDD Round 2: Fix errors - Add setters and getters for Rectangle width and height

--- src/main/java/com/selab/AreaCalculator/Rectangle.java
+++ src/main/java/com/selab/AreaCalculator/Rectangle.java
@@ -1,14 +1,36 @@
 package com.selab.AreaCalculator;
 
 public class Rectangle {
-	private final int width;
-	private final int height;
+	private int width;
+	private int height;
 
 	public Rectangle(int width, int height) throws IllegalArgumentException {
 		if (width <= 0 || height <= 0)
 			throw new IllegalArgumentException("Width or height should be positive.");
 
 		this.width = width;
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setWidth(int width) {
+		if (width <= 0)
+			throw new IllegalArgumentException("Width should be positive.");
+
+		this.width = width;
+	}
+
+	public void setHeight(int height) {
+		if (height <= 0)
+			throw new IllegalArgumentException("Height should be positive.");
+
 		this.height = height;
 	}
 
